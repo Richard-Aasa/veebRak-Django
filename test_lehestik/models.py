a707842c5b84506d2a10cf0e84f5f69886e0fe26
Madis Uibo <madis.uibo@gmail.com>
2017-11-05 18:49:28 +0200

    Dok_Meta model

--- test_lehestik/models.py
+++ test_lehestik/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Dok_Meta(models.Model):
+    kood = models.CharField(max_length=255)
+    korpus = models.CharField(max_length=255)
+    tekstikeel = models.CharField(max_length=25)
+    tekstityyp = models.CharField(max_length=25)
+    elukoht = models.CharField(max_length=25)
+    taust = models.CharField(max_length=25)
+    vanus = models.CharField(max_length=25)
+    sugu = models.CharField(max_length=10)
+    emakeel = models.CharField(max_length=25)
+    kodukeel = models.CharField(max_length=25)
+    keeletase = models.CharField(max_length=5)
+    haridus = models.CharField(max_length=10)
+    abivahendid = models.CharField(max_length=10)
