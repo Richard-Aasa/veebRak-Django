83778edefaae92047a16f7fb6f9c455502b2115b
Janek K <janek.kossinski@gmail.com>
2017-11-05 18:03:04 +0200

    commit hierrarhia korrastus

--- app/views.py
+++ app/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Index',
+        }
+    )
+#HttpResponse("Hello, world. You're at the polls index.")
+
+def manyToManyTable(request):
+    return render(
+        request,
+        'app/manyToManyTable.html',
+        {
+            'title':'manytomany',
+        }
+    )
+    #return httpResponse("todo manytomanytables")
--- test_lehestik/views.py
+++ test_lehestik/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 
 
 def index(request):
-    array = ['test1', 'test2']
+    array = ['test1', 'test2', 'test2', 'test2', 'test2','test2' ,'test2', 'test2', 'test2','test2']
     template = loader.get_template('test_lehestik/index.html')
     context = {
         'array': array,
