8cfefc17395921ed0b47c3c13c5d8bd677d8743b
Richard Aasa <Richard Aasa>
2017-11-05 09:17:02 +0200

    Added model, database migration

--- README.md
+++ README.md
@@ -29,3 +29,22 @@ Väljumiseks SQLite seest
 ```
 .exit
 ```
+
+## Mudelid
+Mudelid asuvad ./app/models.py kaustas
+Peale mudeli loomist tuleb see migreerida andmebaasi
+Alguses loome n.ö juhendi migreerimiseks
+```
+python manage.py makemigrations app
+```
+Vastus peaks tulema kujul
+```
+app/migrations/0001_initial.py
+```
+Seejärel teostame migratsiooni eelmise juhendi alusel
+```
+python manage.py sqlmigrate app 0001
+python manage.py migrate
+```
+
+
--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Births(models.Model):
+    county = models.CharField(max_length=200)
+    y2005 = models.IntegerField(default=0)
+    y2006 = models.IntegerField(default=0)
+    y2007 = models.IntegerField(default=0)
+    y2008 = models.IntegerField(default=0)
+    y2009 = models.IntegerField(default=0)
+    y2010 = models.IntegerField(default=0)
+    y2011 = models.IntegerField(default=0)
+    y2012 = models.IntegerField(default=0)
+    y2013 = models.IntegerField(default=0)
+    y2014 = models.IntegerField(default=0)
+    y2015 = models.IntegerField(default=0)
+
